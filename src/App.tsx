e938caa2bfaa1fd5b473db50be2d551bb94ad3c9
Elio1995 <mehmetielio95@gmail.com>
2022-09-28 16:48:16 +0200

    Removed some unnecessary files and done some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
+import { useEffect } from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import logo from "../images/logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [expenses, setExpenses] = useState([]);
 
-  return <div className="App">Hey</div>;
+  function getExpenseList() {
+    return fetch(`http://localhost:3000/expenses`)
+      .then((response) => response.json())
+      .then((data) => {
+        setExpenses(data);
+      });
+  }
+
+  useEffect(() => {
+    getExpenseList();
+  }, []);
+  console.log(expenses);
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          backgroundColor: "hsl(10, 79%, 65%)",
+          display: "grid",
+          gridTemplateColumns: "1fr 50px",
+          padding: "20px",
+          borderRadius: "15px",
+        }}
+      >
+        <div style={{ textAlign: "start" }}>
+          <p>My balance</p>
+          <p>300$</p>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            placeContent: "center",
+          }}
+        >
+          <img src={logo}></img>
+        </div>
+      </div>
+      <div
+        style={{
+          marginTop: "20px",
+          textAlign: "start",
+          color: "black",
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "15px",
+        }}
+      >
+        <p>Spending - Last 7 days</p>
+        {expenses.map((expense) => {
+          return (
+            <>
+              <p>{expense.amount}</p>
+              <p>{expense.day}</p>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
