ae40c04c7b899e4929e0388a86d162e5db92a98b
Elio1995 <mehmetielio95@gmail.com>
2022-09-29 18:28:01 +0200

    Added footer, sum and % in the UI

--- data.tsx
+++ data.tsx
@@ -1,36 +1,29 @@
 export const Expenses = [
   {
-    id: 1,
     day: "mon",
     amount: 17.45,
   },
   {
-    id: 2,
     day: "tue",
     amount: 34.91,
   },
   {
-    id: 3,
     day: "wed",
     amount: 52.36,
   },
   {
-    id: 4,
     day: "thu",
     amount: 31.07,
   },
   {
-    id: 5,
     day: "fri",
     amount: 23.39,
   },
   {
-    id: 6,
     day: "sat",
     amount: 43.28,
   },
   {
-    id: 7,
     day: "sun",
     amount: 25.48,
   },
--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ export interface DATA {
 }
 
 const sum = Expenses.map((expense) => expense.amount).reduce((a, b) => a + b);
-console.log(sum);
 
 function App() {
   return (
@@ -53,6 +52,31 @@ function App() {
       >
         <p>Spending - Last 7 days</p>
         <BarChart />
+        <hr className="mt-6" />
+        <div
+          className="mt-6"
+          style={{ display: "grid", gridTemplateColumns: "1fr 150px" }}
+        >
+          <div>
+            <p style={{ color: "grey", fontWeight: "500" }}>Total this month</p>
+            <p
+              style={{ fontSize: "40px", marginTop: "10px", fontWeight: "500" }}
+            >
+              ${sum}
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-end" style={{ fontWeight: "500" }}>
+              +2.4%
+            </p>
+            <p
+              className="text-end"
+              style={{ color: "grey", fontWeight: "500" }}
+            >
+              from last month
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
