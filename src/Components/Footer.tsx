75449a68ed42c3eaf5c0d651ce2168bbb6bdec24
Elio1995 <mehmetielio95@gmail.com>
2022-10-03 08:47:13 +0200

    Added tailwind css to footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,23 +4,14 @@ import "../App.css";
 function Footer() {
   const sum = Expenses.map((expense) => expense.amount).reduce((a, b) => a + b);
   return (
-    <div
-      className="mt-6"
-      style={{ display: "grid", gridTemplateColumns: "1fr 150px" }}
-    >
+    <div className="mt-6 grid grid-cols-[1fr_150px]">
       <div>
-        <p style={{ color: "grey", fontWeight: "500" }}>Total this month</p>
-        <p style={{ fontSize: "40px", marginTop: "10px", fontWeight: "500" }}>
-          ${sum}
-        </p>
+        <p className="font-medium text-slate-400">Total this month</p>
+        <p className="font-medium mt-2.5 text-4xl">${sum}</p>
       </div>
       <div className="mt-3">
-        <p className="text-end" style={{ fontWeight: "500" }}>
-          +2.4%
-        </p>
-        <p className="text-end" style={{ color: "grey", fontWeight: "500" }}>
-          from last month
-        </p>
+        <p className="text-end font-medium">+2.4%</p>
+        <p className="text-end font-medium text-slate-400">from last month</p>
       </div>
     </div>
   );
